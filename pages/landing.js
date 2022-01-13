2c88a0f37c6e1297ff99f2e1cba54dadec552967
lucianovilela <lucianovilela@gmail.com>
2022-01-13 23:08:24 +0000

    .

--- pages/landing.js
+++ pages/landing.js
@@ -34,7 +34,7 @@ export default function LandingPage(props) {
       <Header
         color="transparent"
         routes={dashboardRoutes}
-        brand="NextJS Material Kit"
+        brand="Mariana Guimarães - \u{2696}"
         rightLinks={<HeaderLinks />}
         fixed
         changeColorOnScroll={{
